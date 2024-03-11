9997ca228cd93ba9044bf51faf3e5c5be8e8a448
Saksham329 <Shivamssharma398@gmail.com>
2024-03-11 23:29:59 +0530

    updated till level

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 let colors = ["green","red","yellow","blue"]
 let gamepattern = [];
 let userclickedpattern = []
-let level = 
+let level = 0 ; 
+let started = false;
+
+$(document).on("keydown",function(){
+    if (!started){
+        $("#level-title").text("level "+ level)
+        colorsequence()
+        started = true
+        }
+})
 
 $(".btn").on("click",function(){
     let userchosencolor = $(this).attr("id")
     userclickedpattern.push(userchosencolor)
     playsound(userchosencolor)
     animatepress(userchosencolor)
+    checkanswer(userclickedpattern.length-1)
 })
 ``
-$(document).on("keydown",function(){colorsequence()})
+
 
 function colorsequence(){
+    level++
+    userclickedpattern = []
+    $("#level-title").text("level "+ level)
     let randomnumber = Math.floor(Math.random()*4)
     let randomchosencolor = colors[randomnumber];
     gamepattern.push(randomchosencolor)
@@ -23,6 +36,18 @@ function colorsequence(){
 
 }
 /* function that will be used to play sound*/
+
+function checkanswer(currentlevel){
+    if(userclickedpattern[currentlevel] === gamepattern[currentlevel])
+    {console.log("success");
+    if(userclickedpattern.length === gamepattern.length){
+        setTimeout(function(){colorsequence()},1000)
+    }
+}
+    else{console.log("wrong")}
+}
+
+
 function playsound(name){
     let audio1 = new Audio("sounds/"+ name +".mp3")
     audio1.play()
@@ -34,5 +59,4 @@ function animatepress(currentcolor){
     setTimeout(function() {$("." + currentcolor).removeClass("pressed")},100)
 
 }
-
-
+colorsequence()
